5ec8521f3838d812a81124cba9043d29cc236e08
Amanullah Aman <amancse.aiub@gmail.com>
2016-11-30 14:47:02 +0600

    Javascript Scope

---
+++ js/14.scope.js
@@ -0,0 +1,13 @@
+myFunction();
+document.getElementById('sc1').innerHTML =" Type is: "+ typeof carName;
+
+function myFunction(){
+    var carName = "BMW";
+}
+
+
+var person = "Amanullah Aman" ;
+personFun();
+function personFun(){
+  document.getElementById('sc2').innerHTML ="Name is: "+ person + " Type is : "+ typeof person;
+}
