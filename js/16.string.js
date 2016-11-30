a9ac82d057570080d4d6d67ed39023141b20c0dc
Amanullah Aman <amancse.aiub@gmail.com>
2016-11-30 16:39:57 +0600

    JavaScript String

---
+++ js/16.string.js
@@ -0,0 +1,10 @@
+document.getElementById('st1').innerHTML = "It's Alright";
+document.getElementById('st2').innerHTML = "It's 'John'";
+document.getElementById('st3').innerHTML = "The length of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' = "+ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.length;
+document.getElementById('st4').innerHTML = "It\"s Aman";
+
+var x = 'Aman';
+var y = new String('Aman');
+
+document.getElementById('st5').innerHTML = x + "Type is : "+ typeof x;
+document.getElementById('st6').innerHTML = y + "Type is : "+ typeof y;
