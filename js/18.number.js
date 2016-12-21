db912cde9205fac5190b7cebfce795044074f369
Amanullah Aman <amancse.aiub@gmail.com>
2016-12-21 13:31:18 +0600

    Number in Javascript. decimal, hexadecimal, octal, binary

---
+++ js/18.number.js
@@ -0,0 +1,9 @@
+var equ = (0.2 * 10 + 0.1 * 10) / 10 ;
+document.getElementById('num1').innerHTML = "(0.2 * 10 + 0.1 * 10) / 10 = "+equ ;
+
+
+var num = 128 ;
+document.getElementById('num2').innerHTML = num+" = ???<br>In Decimal: "+num +
+"<br> In Hexadecimal: "+num.toString(16) +
+"<br> In Octal : "+num.toString(8) +
+"<br>In Binary: "+num.toString(2);
