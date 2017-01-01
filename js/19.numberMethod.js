cbb63c128508f900bfdb4e0668aa8dd17f019951
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-01 13:17:36 +0600

    Javascript exponential

--- js/19.numberMethod.js
+++ js/19.numberMethod.js
@@ -1,3 +1,9 @@
 var num1 = 100;
 document.getElementById('num1').innerHTML = 'toString() Return a number as string'+ num1.toString();
 document.getElementById('num2').innerHTML = '(100+50)toString() Return a number as string = '+ (100 + 50).toString();
+
+document.getElementById('h2').innerHTML = 'toExponential() returns a string, with a number rounded and written using exponential notation.';
+
+var num2 = 9.652 ;
+document.getElementById('num3').innerHTML = num2.toExponential(2);
+document.getElementById('num4').innerHTML = num2.toExponential(4);
