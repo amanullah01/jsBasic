17c5bc37ea2734f54927e765a19ce40f3f58552f
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-01 13:14:08 +0600

    Javascript number method

---
+++ js/19.numberMethod.js
@@ -0,0 +1,3 @@
+var num1 = 100;
+document.getElementById('num1').innerHTML = 'toString() Return a number as string'+ num1.toString();
+document.getElementById('num2').innerHTML = '(100+50)toString() Return a number as string = '+ (100 + 50).toString();
