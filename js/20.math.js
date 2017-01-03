1e695164875c0e524d9f157bed0ae0fc11c1f476
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-03 16:40:44 +0600

    Javascript Math object. calculate pi value using math.pi

---
+++ js/20.math.js
@@ -0,0 +1,3 @@
+document.getElementById('h1').innerHTML = 'The JavaScript Math object allows you to perform mathematical tasks on numbers.';
+document.getElementById('m1').innerHTML = 'Value of pi: Math.PI = '+ Math.PI;
+document.getElementById('m2').innerHTML = 'Value of pi: Math.PI = '+ Math.PI.toFixed(4);
