a270e6aa8ce24d0609a10b4fc026daa6518b65d9
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-03 17:13:46 +0600

    Math round power sqrt

--- js/20.math.js
+++ js/20.math.js
@@ -5,3 +5,11 @@ document.getElementById('m2').innerHTML = 'Value of pi: Math.PI = '+ Math.PI.toF
 document.getElementById('h2').innerHTML = 'Math.round(x) returns the value of x rounded to its nearest integer:';
 document.getElementById('m3').innerHTML = 'Value of pi: 4.4 round = '+ Math.round(4.4);
 document.getElementById('m4').innerHTML = 'Value of pi: 4.6 round = '+ Math.round(4.6);
+
+document.getElementById('h3').innerHTML = 'Math.pow(x,y) returns the value of x to the power y:';
+document.getElementById('m5').innerHTML = '8 to the power 2  = '+ Math.pow(8,2);
+document.getElementById('m6').innerHTML = '8 to the power 3  = '+ Math.pow(8,3);
+
+document.getElementById('h4').innerHTML = 'Math.sqrt(x) returns the square root of x:';
+document.getElementById('m7').innerHTML = '64 sqrt  = '+ Math.sqrt(64);
+document.getElementById('m8').innerHTML = '64 sqrt  = '+ Math.sqrt(125);
