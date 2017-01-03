b431eb513a562796a1b9bfdebab88f63329c5bef
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-03 16:59:19 +0600

    javascript math.round

--- js/20.math.js
+++ js/20.math.js
@@ -1,3 +1,7 @@
 document.getElementById('h1').innerHTML = 'The JavaScript Math object allows you to perform mathematical tasks on numbers.';
 document.getElementById('m1').innerHTML = 'Value of pi: Math.PI = '+ Math.PI;
 document.getElementById('m2').innerHTML = 'Value of pi: Math.PI = '+ Math.PI.toFixed(4);
+
+document.getElementById('h2').innerHTML = 'Math.round(x) returns the value of x rounded to its nearest integer:';
+document.getElementById('m3').innerHTML = 'Value of pi: 4.4 round = '+ Math.round(4.4);
+document.getElementById('m4').innerHTML = 'Value of pi: 4.6 round = '+ Math.round(4.6);
