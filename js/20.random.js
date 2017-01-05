1730e8b6a6f570057f0fcac6d9e1b305cb51288e
Amanullah Aman <amancse.aiub@gmail.com>
2017-01-05 15:51:41 +0600

    Javascript random value

---
+++ js/20.random.js
@@ -0,0 +1,4 @@
+document.getElementById('r1').innerHTML = 'Random value between 0 to 1 = '+ Math.random();
+document.getElementById('r2').innerHTML = 'Random value between 0 to 9 = '+ Math.floor(Math.random() * 10);
+document.getElementById('r3').innerHTML = 'Random value between 0 to 99 = '+ Math.floor(Math.random() * 100);
+document.getElementById('r4').innerHTML = 'Random value between 0 to 10000 = '+ Math.floor(Math.random() * 1000) + 1;
